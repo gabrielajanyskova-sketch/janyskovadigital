439f9ec73d10421c1df25647964979db8e86b21f
gabrielajanyskova-sketch <gabriela.janyskova@email.cz>
2026-05-26 06:42:29 +0200

    Delete functions/api directory

--- functions/api/auth.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const clientId = "Ov231lhLF6oRmHXEduvv"; // Tvoje Client ID z GitHubu
-  const redirectUri = "https://janyskovadigital.cz/api/callback";
-  
-  return Response.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=repo`,
-    302
-  );
-}
